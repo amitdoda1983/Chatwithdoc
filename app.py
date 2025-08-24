fa1210b985caa098c60021e2399336bd0e28094d
Amit Doda <37932202+amitdoda1983@users.noreply.github.com>
2025-08-24 16:03:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+    
 import os
 from dotenv import find_dotenv, load_dotenv
 import openai
